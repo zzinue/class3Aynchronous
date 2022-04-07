bbab3f82bc2cf1ea892c13435912970206a26b51
zzinue <zzinue@gmail.com>
2022-04-07 10:58:23 -0500

    entire process built, it only the many cars function to work

--- fixassembly.js
+++ fixassembly.js
@@ -1,55 +1,55 @@
 const carObject = {
-        chassis: {
-            headliners: false,
-            assembly: false,
-        },
-        body: {
-            doors: false,
-            windows: 0,
-            sanding: false,
-            paintColor: '',
-            wheels: 0,
-        },
-        prepareToSell: {
-            fuel: 0,
-            /* prepare to sell */
-            oil: 0,
-            /* prepare to sell */
-            clean: false,
-            /* prepare to sell */
-            location: 'factory',
-            /* send to the agency */
-        },
-        onTheAgency: {
-            inventoried: false,
-            /*   receive on the agency */
-            exhibited: false,
-            /*  receive on the agency */
-        },
-    }
-    //Assembling the chassis:
+    chassis: {
+        headliners: false,
+        assembly: false,
+    },
+    body: {
+        doors: false,
+        windows: 0,
+        sanding: false,
+        paintColor: '',
+        wheels: 0,
+    },
+    prepareToSell: {
+        fuel: 0,
+        /* prepare to sell */
+        oil: 0,
+        /* prepare to sell */
+        clean: false,
+        /* prepare to sell */
+        location: 'factory',
+        /* send to the agency */
+    },
+    onTheAgency: {
+        inventoried: false,
+        /*   receive on the agency */
+        exhibited: false,
+        /*  receive on the agency */
+    },
+}
+const randomTime = () => Math.random() * (3000 - 1000) + 1000;
+//Assembling the chassis:
 const buildChassisAction = async() => {
-        console.log('car 1: Chassis:started to prepare');
-        const car = {}
+        const car = ''
         const carElements = carObject.chassis
-        const addHeadliners = (car) => {
+        const addHeadliners = () => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
-                    (carElements.headliners === false) ? result = `headliners:${carElements.headliners = true}`: reject('headliners cannot be added')
+                    (!carElements.headliners) ? result = `headliners:${carElements.headliners = true}`: reject('headliners cannot be added')
                     console.log(result);
                     resolve(result)
-                }, 1000)
+                }, randomTime())
             })
         }
-        const assemblyCar = (car) => {
+        const assemblyCar = () => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
                     (carElements.assembly === false) ? result = `assembly:${carElements.assembly = true}`: reject('assembly cannot be added')
-                    let resultObject = Object.entries(carObject.chassis)
                     console.log(result);
+                    let resultObject = Object.entries(carObject.chassis)
                     console.log(resultObject);
                     resolve(result)
-                }, 2000)
+                }, randomTime())
             })
         }
         try {
@@ -61,46 +61,46 @@ const buildChassisAction = async() => {
     }
     //Assembling the bodyWork:
 const buildBodyWorkAction = async() => {
-        console.log('car 1: BodyWork started to prepare');
+        console.log('BodyWork started to prepare');
         const car = {}
         const carElementsBody = carObject.body
-        const addDoors = (doors) => {
+        const addDoors = () => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
                     (carElementsBody.doors === false) ? result = `doors:${carElementsBody.doors = 4}`: reject('doors cannot be added')
                     console.log(result);
                     resolve(result)
-                }, 1000)
+                }, randomTime())
             })
         }
-        const addWindows = (car) => {
+        const addWindows = () => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
                     (carElementsBody.windows === 0) ? result = `windows:${carElementsBody.windows = 4}`: reject('windows cannot be added')
                     console.log(result);
                     resolve(result)
-                }, 1000)
+                }, randomTime())
             })
         }
-        const addSanding = (car) => {
+        const addSanding = () => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
                     (carElementsBody.sanding === false) ? result = `sanding:${carElementsBody.sanding = true}`: reject('sanding cannot be added')
                     console.log(result);
                     resolve(result)
-                }, 1000)
+                }, randomTime())
             })
         }
         const addPaintColor = () => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
-                    (carElementsBody.paintColor === '') ? result = `paint color:${carElementsBody.paintColor = 'red'}`: reject('paint color cannot be added')
+                    (carElementsBody.paintColor === '') ? result = `paint color:${carElementsBody.paintColor = 'painted'}`: reject('paint color cannot be added')
                     console.log(result);
                     resolve(result)
-                }, 1000)
+                }, randomTime())
             })
         }
-        const addWheels = (Bodywork) => {
+        const addWheels = () => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
                     (carElementsBody.wheels === 0) ? result = `wheels:${carElementsBody.wheels = 4}`: reject('wheels cannot be added')
@@ -108,7 +108,7 @@ const buildBodyWorkAction = async() => {
                     result = Object.entries(carObject.body)
                     console.log(result);
                     resolve(result)
-                }, 1000)
+                }, randomTime())
             })
         }
         try {
@@ -123,7 +123,6 @@ const buildBodyWorkAction = async() => {
     }
     //Preparing to Sell:
 const prepareToSellAction = async() => {
-        console.log('car 1: Preparing to sell');
         const car = {}
         const carElementsToSell = carObject.prepareToSell
         const addFuel = (car) => {
@@ -132,7 +131,7 @@ const prepareToSellAction = async() => {
                     (carElementsToSell.fuel === 0) ? result = `fuel:${carElementsToSell.fuel = 'Full'}`: reject('fuel cannot be added')
                     console.log(result);
                     resolve(result)
-                }, 1000)
+                }, randomTime())
             })
         }
         const addOil = (car) => {
@@ -141,7 +140,7 @@ const prepareToSellAction = async() => {
                     (carElementsToSell.oil === 0) ? result = `oil:${carElementsToSell.oil = 'Full'}`: reject('oil cannot be added')
                     console.log(result);
                     resolve(result)
-                }, 1000)
+                }, randomTime())
 
             })
         }
@@ -149,11 +148,11 @@ const prepareToSellAction = async() => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
                     (carElementsToSell.clean === false) ? result = `clean:${carElementsToSell.clean = true}`: reject('clean cannot be added')
-                    console.log('Car 1: Car is clean and ready to sent to the agency!');
+                    console.log('Car is clean and ready to sent to the agency!');
                     result = Object.entries(carObject.prepareToSell)
                     console.log(result);
                     resolve(result)
-                }, 2000)
+                }, randomTime())
             })
 
         }
@@ -161,11 +160,11 @@ const prepareToSellAction = async() => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
                     (carElementsToSell.location === 'factory') ? result = carElementsToSell.location = 'agency': reject('location cannot be added')
-                    console.log('Car: arrived to the agency!');
+                    console.log('Car arrived to the agency!');
                     result = Object.entries(carObject.prepareToSell)
                     console.log(result);
                     resolve(result)
-                }, 4000)
+                }, randomTime())
             })
         }
         try {
@@ -179,7 +178,7 @@ const prepareToSellAction = async() => {
     }
     // Car received on the agency: 
 const receivedToTheAgencyAction = async() => {
-        console.log('car 1: Now on the agency!');
+        console.log('Now on the agency!');
         const car = {}
         let objectCar = carObject
         const carElementsToAgency = carObject.onTheAgency
@@ -189,7 +188,7 @@ const receivedToTheAgencyAction = async() => {
                     (carElementsToAgency.inventoried === false) ? result = `inventoried:${carElementsToAgency.inventoried = true}`: reject('Car not yet inventoried')
                     console.log(result);
                     resolve(result)
-                }, 1000)
+                }, randomTime())
             })
         }
         const isInExhibition = (car) => {
@@ -199,9 +198,9 @@ const receivedToTheAgencyAction = async() => {
                     console.log(result);
                     result = Object.entries(carObject.onTheAgency)
                     console.log(result);
-                    console.log('Car 1: Car is now on exhibition and completed!:');
+                    console.log('Car is now on exhibition and completed!:');
                     resolve(result)
-                }, 1000)
+                }, randomTime())
                 setTimeout(() => {
                     console.log('\n All features:');
                     objectCar = Object.entries(carObject)
@@ -218,20 +217,21 @@ const receivedToTheAgencyAction = async() => {
         }
     }
     //process complete and integrated:
-const builtCar = async() => {
-        try {
-            const chassisBuilt = await buildChassisAction();
-            const bodyWorkBuile = await buildBodyWorkAction(chassisBuilt)
-            const preparedToSell = await prepareToSellAction(bodyWorkBuile)
-            return await receivedToTheAgencyAction(preparedToSell)
-        } catch (error) {
-            console.log('an error ocurred:', error);
-        }
+const carBuiltEntireProcess = async(cardNumber) => {
+        const chassisBuilt = await buildChassisAction();
+        console.log(`Chasis ${cardNumber} built!`);
+        const bodyWorkBuile = await buildBodyWorkAction(chassisBuilt)
+        console.log(`Body work ${cardNumber} built!`);
+        const preparedToSell = await prepareToSellAction(bodyWorkBuile)
+        console.log(`Car ${cardNumber} is preparing to sell!`);
+        const readyToSell = await receivedToTheAgencyAction(preparedToSell)
+        console.log(`Car ${cardNumber} is on exhibition`, );
     }
     //function to sell many cars: 
-const buildCars = (numberOfCars) => {
-    for (let i = 0; i < numberOfCars; i++) {
-        builtCar()
+const buildManyCars = (numberOfCars) => {
+    while (numberOfCars > 0) {
+        carBuiltEntireProcess(numberOfCars);
+        --numberOfCars;
     }
-}
-buildCars(2)
+};
+buildManyCars(2)
